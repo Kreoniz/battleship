3d60a3d03c55afa90231ea88f19de58f1de8a0ce
Kreoniz <kreoniz@mail.ru>
2024-06-02 09:49:22 +0600

    Add player class and test coverage

---
+++ tests/player.test.js
@@ -0,0 +1,8 @@
+import { Player } from '../player';
+
+test('if gameboard objects of each player are different', () => {
+  const player1 = new Player();
+  const player2 = new Player();
+
+  expect(player1.gameboard).not.toBe(player2.gameboard);
+});
