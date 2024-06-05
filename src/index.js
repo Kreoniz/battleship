5fcaec6b8fa8944aa2a20c4cb96b8d50f1143644
Kreoniz <kreoniz@mail.ru>
2024-06-05 09:06:37 +0600

    Add css-loader and render the board in browser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import { Player } from './scripts/player.js';
+import './styles/styles.css';
 
 console.log('Hello');
-const board = document.querySelector('#board');
-console.log(board);
 
 const player = new Player();
 
 console.log(player);
+console.log(player.gameboard);
+
+const board = document.querySelector('#board');
+
+for (let i = 0; i < 10; i += 1) {
+  const rowDiv = document.createElement('div');
+  rowDiv.classList.add('row');
+
+  for (let j = 0; j < 10; j += 1) {
+    const cellDiv = document.createElement('div');
+    cellDiv.classList.add('cell');
+    cellDiv.dataset.x = j;
+    cellDiv.dataset.y = i;
+
+    rowDiv.appendChild(cellDiv);
+  }
+
+  board.appendChild(rowDiv);
+}
