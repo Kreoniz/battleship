9d8c5053f8de11ebf72c5c941b195d7808b4faa3
Kreoniz <kreoniz@mail.ru>
2024-06-07 08:48:23 +0600

    Update webpack config

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,23 +2,27 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
-    },
-    plugins: [new HtmlWebpackPlugin({
-        inject: false,
-        title: 'Battleship',
-        filename: 'index.html',
-        template: './src/index.html',
-    })],
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: false,
+      title: 'Battleship',
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+  ],
 };
